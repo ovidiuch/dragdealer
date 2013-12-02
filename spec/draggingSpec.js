cf6f819103af941ef785cd98a745ecdb0698c332
Ovidiu Chereches <skidding@gmail.com>
2013-12-02 20:41:49 +0200

    Add tests for sliding to steps #1

--- spec/draggingSpec.js
+++ spec/draggingSpec.js
@@ -52,4 +52,37 @@ describe("When dragging a Dragdealer instance", function() {
     jasmine.Clock.tick(3000);
     expect('simple-slider').toHavePosition(250, 0);
   });
+
+  it("Should slide handle to closest step after releasing drag", function() {
+    // Considering the simple slider has a wrapper of 500px width and a handle
+    // of 100px width, the step positions will be 80, 160, 240, 320 and 400
+    helpers.initDragdealer('simple-slider', {
+      slide: false,
+      steps: 6
+    });
+
+    helpers.dragTo('simple-slider', 100, 0);
+    helpers.drop('simple-slider');
+    expect('simple-slider').toHavePosition(100, 0);
+    jasmine.Clock.tick(3000);
+    expect('simple-slider').toHavePosition(80, 0);
+
+    helpers.dragTo('simple-slider', 50, 0);
+    helpers.drop('simple-slider');
+    expect('simple-slider').toHavePosition(50, 0);
+    jasmine.Clock.tick(3000);
+    expect('simple-slider').toHavePosition(80, 0);
+
+    helpers.dragTo('simple-slider', 350, 0);
+    helpers.drop('simple-slider');
+    expect('simple-slider').toHavePosition(350, 0);
+    jasmine.Clock.tick(3000);
+    expect('simple-slider').toHavePosition(320, 0);
+
+    helpers.dragTo('simple-slider', 210, 0);
+    helpers.drop('simple-slider');
+    expect('simple-slider').toHavePosition(210, 0);
+    jasmine.Clock.tick(3000);
+    expect('simple-slider').toHavePosition(240, 0);
+  });
 });
