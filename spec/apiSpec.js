7eaa6c248c947ae2621b3e65f3b8ad1c2c275485
Ovidiu Chereches <skidding@gmail.com>
2013-12-15 21:33:08 +0200

    Expect callbacks to be called after setting value through API #4

--- spec/apiSpec.js
+++ spec/apiSpec.js
@@ -53,44 +53,72 @@ describe("Dragdealer API", function() {
   });
 
   it("should slide handle to position on setValue(x, y)", function() {
-    var dragdealer = helpers.initDragdealer('square-slider', {
+    var callback = jasmine.createSpy(),
+        animationCallback = jasmine.createSpy(),
+        dragdealer = helpers.initDragdealer('square-slider', {
       horizontal: true,
-      vertical: true
+      vertical: true,
+      callback: callback,
+      animationCallback: animationCallback
     });
 
     dragdealer.setValue(1, 0.5);
     jasmine.Clock.tick(3000);
+
     expect('square-slider').toHavePosition(400, 200);
+    expect(callback).toHaveBeenCalledWith(1, 0.5);
+    expect(animationCallback).toHaveBeenCalledWith(1, 0.5);
   });
 
   it("should snap handle to position on setValue(x, y, true)", function() {
-    var dragdealer = helpers.initDragdealer('square-slider', {
+     var callback = jasmine.createSpy(),
+        animationCallback = jasmine.createSpy(),
+        dragdealer = helpers.initDragdealer('square-slider', {
       horizontal: true,
-      vertical: true
+      vertical: true,
+      callback: callback,
+      animationCallback: animationCallback
     });
 
     dragdealer.setValue(1, 0.5, true);
     jasmine.Clock.tick(25);
+
     expect('square-slider').toHavePosition(400, 200);
+    expect(callback).toHaveBeenCalledWith(1, 0.5);
+    expect(animationCallback).toHaveBeenCalledWith(1, 0.5);
   });
 
   it("should slide handle to step position on setStep(x, y)", function() {
-    var dragdealer = helpers.initDragdealer('simple-slider', {
-      steps: 6
+     var callback = jasmine.createSpy(),
+        animationCallback = jasmine.createSpy(),
+        dragdealer = helpers.initDragdealer('simple-slider', {
+      steps: 6,
+      callback: callback,
+      animationCallback: animationCallback
     });
 
     dragdealer.setStep(2, 0);
     jasmine.Clock.tick(3000);
+
     expect('simple-slider').toHavePosition(80, 0);
+    expect(callback).toHaveBeenCalledWith(0.2, 0);
+    expect(animationCallback).toHaveBeenCalledWith(0.2, 0);
   });
 
   it("should snap handle to step position on setStep(x, y, true)", function() {
-    var dragdealer = helpers.initDragdealer('simple-slider', {
-      steps: 6
+     var callback = jasmine.createSpy(),
+        animationCallback = jasmine.createSpy(),
+        dragdealer = helpers.initDragdealer('simple-slider', {
+      steps: 6,
+      callback: callback,
+      animationCallback: animationCallback
     });
 
     dragdealer.setStep(2, 0, true);
     jasmine.Clock.tick(25);
+
     expect('simple-slider').toHavePosition(80, 0);
+    expect(callback).toHaveBeenCalledWith(0.2, 0);
+    expect(animationCallback).toHaveBeenCalledWith(0.2, 0);
   });
 });
