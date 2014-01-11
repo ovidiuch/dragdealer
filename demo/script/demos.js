22af524c241fe2897f757fec70f3ed4c1a059577
Ovidiu Chereches <skidding@gmail.com>
2014-01-11 20:45:50 +0200

    Add first demo #9

--- demo/script/demos.js
+++ demo/script/demos.js
@@ -1,3 +1,10 @@
 $(function() {
   new Dragdealer('demo-simple-slider');
+
+  var tooltipValue = $('#just-a-slider .tooltip .value').get(0);
+  new Dragdealer('just-a-slider', {
+    animationCallback: function(x, y) {
+      tooltipValue.innerHTML = Math.round(x * 100);
+    }
+  });
 });
