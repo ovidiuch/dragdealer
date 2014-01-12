30bc25a9c588bd5107cb962b5fb1da469161f09d
Ovidiu Chereches <skidding@gmail.com>
2014-01-12 19:22:14 +0200

    Add content scroller demo #9

--- demo/script/demos.js
+++ demo/script/demos.js
@@ -7,6 +7,17 @@ $(function() {
     }
   });
 
+  var availHeight = $('.content-body').outerHeight() -
+                    $('.content-mask').outerHeight();
+  new Dragdealer('content-scroller', {
+    horizontal: false,
+    vertical: true,
+    yPrecision: availHeight,
+    animationCallback: function(x, y) {
+      $('.content-body').css('margin-top', -y * availHeight);
+    }
+  });
+
   var slideToUnlockOld = new Dragdealer('slide-to-unlock-old', {
     steps: 2,
     callback: function(x, y) {
