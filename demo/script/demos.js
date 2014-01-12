bfd742bdadcd886d418712f53fb9d93bd84d8c55
Ovidiu Chereches <skidding@gmail.com>
2014-01-12 15:58:28 +0200

    Simplify first demo #9

--- demo/script/demos.js
+++ demo/script/demos.js
@@ -1,10 +1,9 @@
 $(function() {
   new Dragdealer('demo-simple-slider');
 
-  var tooltipValue = $('#just-a-slider .tooltip .value').get(0);
   new Dragdealer('just-a-slider', {
     animationCallback: function(x, y) {
-      tooltipValue.innerHTML = Math.round(x * 100);
+      $('#just-a-slider .value').text(Math.round(x * 100));
     }
   });
 
